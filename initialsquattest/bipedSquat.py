31c4696108b57ee9da69f0bf98085c9b6e7fea71
Anirudh Kailaje <kailaje@seas.upenn.edu>
2023-11-10 12:22:13 -0500

    Basic File Structure

--- initialsquattest/bipedSquat.py
+++ initialsquattest/bipedSquat.py
@@ -7,7 +7,6 @@ Mathematically: We track the Center of Mass trajectory on z direction with const
 """
 import pydot
 import numpy as np
-from IPython.display import SVG, display
 
 from pydrake.all import Simulator, DiagramBuilder, AddMultibodyPlantSceneGraph,\
                         Parser, RigidTransform, MeshcatVisualizer, MeshcatVisualizerParams, \
@@ -17,7 +16,9 @@ import planner
 from osc_modified import OperationalSpaceWalkingController
 
 from pydrake.all import StartMeshcat, BasicVector, LogVectorOutput
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, datetime
+
+time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
 #Start the meshcat server
 meshcat = StartMeshcat()
@@ -45,23 +46,25 @@ plant.Finalize()
 
 
 #### Designing the controller ####
+"""
+Here,
+zdes: desired squat Z height in meters
+z_height_desired: A constant source which outputs the desired Z height, given to the com_planner
+com_planner: A controller which tracks the desired Z height for the center of mass
+base_traj_src: A constant source which outputs a constant vector (No movement in the Y direction for the robot), given to the osc
+osc: The operational space controller which tracks the desired Z height and the base_traj_src
+"""
 zdes = 0.8 #desired Z height in meters
-osc = builder.AddSystem(OperationalSpaceWalkingController())
-com_planner = builder.AddSystem(planner.COMPlanner())
 z_height_desired = builder.AddSystem(ConstantVectorSource(np.array([zdes])))
+com_planner = builder.AddSystem(planner.COMPlanner())
 base_traj_src = builder.AddSystem(ConstantValueSource(AbstractValue.Make(BasicVector(np.zeros(1,)))))
-
-
-## Logger ##
+osc = builder.AddSystem(OperationalSpaceWalkingController())
 logger = LogVectorOutput(osc.GetOutputPort("metrics"),builder)
 
-
 #### Wiring ####
-
 #COM wiring
 builder.Connect(z_height_desired.get_output_port(), com_planner.get_com_zdes_input_port())
 builder.Connect(plant.get_state_output_port(), com_planner.get_com_state_input_port())
-
 # OSC wiring
 builder.Connect(com_planner.get_com_traj_output_port(), osc.get_traj_input_port("com_traj"))
 builder.Connect(base_traj_src.get_output_port(), osc.get_traj_input_port("base_joint_traj"))
@@ -71,29 +74,25 @@ builder.Connect(osc.torque_output_port, plant.get_actuation_input_port())
 # Add the visualizer
 vis_params = MeshcatVisualizerParams(publish_period=0.0005)
 MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat, params=vis_params)
-
 #simulate
 diagram = builder.Build()
-display(SVG(pydot.graph_from_dot_data(diagram.GetGraphvizString(max_depth=2))[0].create_svg()))
-
+graph = (pydot.graph_from_dot_data(diagram.GetGraphvizString(max_depth=2))[0].create_svg()))
+with f as open("graph.svg", "w"):
+    f.write(graph)
 ################
 
 sim_time = 1
 simulator = Simulator(diagram)
-simulator.Initialize()
-simulator.set_target_realtime_rate(0.1)
+simulator.Initialize(); simulator.set_target_realtime_rate(0.1)
 
-# Initial State
-plant_context = diagram.GetMutableSubsystemContext(
-    plant, simulator.get_mutable_context())
+# State Description: q = [x, z, planar_roty, left_hip, left_knee, right_hip, right_knee]
+plant_context = diagram.GetMutableSubsystemContext(plant, simulator.get_mutable_context())
+
 q = np.zeros((plant.num_positions(),))
-q[0] = 0
-q[1] = 0.8*1.2
+q[0] = 0; q[1] = 0.8*1.2
 theta = -np.arccos(q[1])
-q[3] = theta/2
-q[4] = -2 * theta
-q[5] = theta
-q[6] = -2 * theta/2
+q[3] = theta/2; q[4] = -2 * theta
+q[5] = theta;   q[6] = -2 * theta/2
 plant.SetPositions(plant_context, q)
 
 # Simulate the robot
@@ -109,5 +108,6 @@ plt.figure()
 plt.plot(t, x)
 plt.figure()
 plt.plot(t, xdot)
+plt.savefig(f"./logs/{time}plots.png")
 plt.show()
 
